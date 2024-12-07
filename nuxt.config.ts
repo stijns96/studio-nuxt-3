1a01667092c548cd529679a5f7d5ac467b64df8a
Stijn <stijn@code.nl>
2024-12-07 10:56:39 +0100

    Add workspace settings

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-11-01',
+  compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
-  extends: ['@nuxt/ui-pro'],
-  modules: ['@nuxt/ui']
-})
+  extends: ["@nuxt/ui-pro"],
+  modules: ["@nuxt/ui"],
+});
