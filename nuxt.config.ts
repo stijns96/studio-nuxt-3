1696bc6f83287b4b469be296b4089211fb2544ff
Stijn <stijn@code.nl>
2024-12-07 11:00:45 +0100

    Add Nuxt Content

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,5 +3,5 @@ export default defineNuxtConfig({
   compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
   extends: ["@nuxt/ui-pro"],
-  modules: ["@nuxt/ui"],
+  modules: ["@nuxt/ui", "@nuxt/content"],
 });
